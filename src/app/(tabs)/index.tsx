d99280eadb9beddaa9d1d4a670f8c1b7da8ed8a9
kedar <kedarnathgubbi.1999@gmail.com>
2024-04-04 18:01:01 -0400

    add image

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,10 +1,11 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 import EditScreenInfo from '../../components/EditScreenInfo';
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
+      <Image style={styles.image} source={{ uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/peperoni.png'}} />
       <Text style={styles.title}>Veg Pizza</Text>
       <Text>$12.99</Text>
     </View>
@@ -13,9 +14,13 @@ export default function TabOneScreen() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor:"white",
+    padding:10,
+    borderRadius:20
+  },
+  image: {
+    width:'100%',
+    aspectRatio:1
   },
   title: {
     fontSize: 20,
