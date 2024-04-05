6071f4921883027d43b21ce8e74e75d6eed348e8
kedar <kedarnathgubbi.1999@gmail.com>
2024-04-05 18:17:04 -0400

    add context cart

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -1,17 +1,15 @@
+import { StyleSheet,Platform } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import { Platform, StyleSheet } from 'react-native';
-
-import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
+import { useContext } from 'react';
+import {useCart} from 'src/providers/CartProvider'
 
-export default function ModalScreen() {
+export default function Cart() {
+  const {items} = useCart()
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Modal</Text>
+    <View >
+      <Text style={styles.title}>{items.length}</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/modal.tsx" />
-
-      {/* Use a light status bar on iOS to account for the black space above the modal */}
       <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
     </View>
   );
---
+++ src/providers/CartProvider.jsx
@@ -0,0 +1,15 @@
+import { createContext, useContext } from "react";
+
+export const CartContext = createContext({});
+
+const CartProvider = ({children}) => {
+    return(
+        <CartContext.Provider value={{items: [1,2,3], onAddItem: ()=> {}}}>
+            {children}
+        </CartContext.Provider>
+    )
+}
+
+export default CartProvider;
+
+export const useCart = () => useContext(CartContext);
